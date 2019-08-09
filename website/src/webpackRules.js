ada2b3681159a0830e435dd7c1eb3220f1ab0edb
cristoMonte <18279185327@163.com>
2019-08-09 17:04:58 +0800

    feat: add mdx

---
+++ website/src/webpackRules.js
@@ -0,0 +1,20 @@
+
+module.exports = [
+  {
+    test: /\.svg$/,
+    use: [{ loader: 'babel-loader' }, { loader: 'react-svg-loader' }]
+  },
+  {
+    test: /\.css$/,
+    use: ['style-loader', 'css-loader']
+  }
+  // {
+  //   test: /\.js$/,
+  //   use: {
+  //     loader: 'babel-loader',
+  //     options: {
+  //       presets: ['@babel/preset-env', '@babel/preset-react']
+  //     }
+  //   }
+  // }
+]
